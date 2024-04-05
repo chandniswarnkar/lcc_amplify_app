283180d5e565d3df7b4086f768419d8354d39f01
chandniswarnkar <chandni.swarnkar@ust.com>
2024-04-05 11:38:40 +0530

    chnges related to launch screeb

--- android/app/src/main/kotlin/com/example/lcc_flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lcc_flutter_app/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.lcc_flutter_app
 
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import org.devio.flutter.splashscreen.SplashScreen // add
+//import org.devio.flutter.splashscreen.SplashScreen // add
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // SplashScreen.show(this)
         // or enable full screen
-        SplashScreen.show(this, true)
+      //  SplashScreen.show(this, true)
         super.onCreate(savedInstanceState)
     }
 }
